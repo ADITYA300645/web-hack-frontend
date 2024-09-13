7d954210bda54d515f1c08beacbcea052fb06b3d
adityayadav3005 <adityayadav300645@gmail.com>
2024-09-13 15:34:24 +0530

    base

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="transition-colors duration-500 dark:bg-[#2B2A33] bg-[#E6E6E6] overflow-x-hidden">
         <Main />
         <NextScript />
       </body>
