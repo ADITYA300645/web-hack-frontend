78c78be2b5d84d35edd60e5944fff7bb75e9fd7c
Ritvik Bharti <bhartiritvik1000@gmail.com>
2024-09-14 03:18:02 -0700

    ToDoPage

---
+++ src/pages/TodoPage/Component/RepoCard/RepoCard.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function RepoCard() {
+  return (
+    <>
+    <div className='h-1/2 w-1/4 flex flex-col justify-center'>
+
+    <div className='h-5/6 bg-cyan-600 w-5/6 bg-black rounded-3xl m-7'>
+
+    
+
+   
+    </div>
+   
+        <p className='ml-26'>
+            Fairy Mate
+        </p>
+    
+    </div>
+    </>
+  )
+}
+
+export default RepoCard
--- src/pages/TodoPage/index.js
+++ src/pages/TodoPage/index.js
@@ -1,17 +1,26 @@
 import React from 'react'
 import NavigationBar from '../../utils/Navigation/NavigationBar'
 import SearchBar from './Component/SearchBar/SearchBar'
+import RepoCard from './Component/RepoCard/RepoCard'
 
 function index() {
   return (
     <div className='h-full w-full justify-center'>
 
         <NavigationBar/>
-        <div className='h-screen w-full bg-custom-color flex flex-col align-center'>
+        <div className='h-screen w-full bg-custom-color flex flex-col items-center'>
 
         <SearchBar/>
-        <div className='  bg-custom-color'>
-
+        <div className='h-full w-11/12 border-2 border-black	 rounded-xl flex mt-7  flex-wrap justify-around'>
+                <RepoCard/>
+                <RepoCard/>
+                <RepoCard/>
+                <RepoCard/>
+                <RepoCard/>
+                <RepoCard/>
+                
+                
+                
         </div>
         </div>
         
